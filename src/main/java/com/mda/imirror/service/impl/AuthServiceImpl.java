d0dd24faf95a95c5ef053e8880715195462c31ba
최현호 <117135776+QAQA0@users.noreply.github.com>
2024-04-16 17:18:39 +0900

    add member enums
    회원가입 로직 구현

--- src/main/java/com/mda/imirror/service/impl/AuthServiceImpl.java
+++ src/main/java/com/mda/imirror/service/impl/AuthServiceImpl.java
@@ -1,13 +1,13 @@
 package com.mda.imirror.service.impl;
 
-import com.mda.imirror.domain.Member;
-import com.mda.imirror.domain.MemberRegisterRequest;
+import com.mda.imirror.domain.entity.Member;
+import com.mda.imirror.domain.enums.MemberRole;
+import com.mda.imirror.domain.request.MemberRegisterRequest;
+import com.mda.imirror.domain.response.MemberRegisterResponse;
 import com.mda.imirror.repository.MemberRepository;
 import com.mda.imirror.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -16,19 +16,21 @@ public class AuthServiceImpl implements AuthService {
     private final MemberRepository memberRepository;
 
     @Override
-    public Member register(MemberRegisterRequest request) {
+    public MemberRegisterResponse register(MemberRegisterRequest request) {
         Member member = Member.builder()
+                .memberId(request.getMemberId())
+                .memberPassword(request.getMemberPassword())
                 .memberName(request.getMemberName())
                 .memberGender(request.getMemberGender())
                 .memberAge(request.getMemberAge())
                 .memberHeight(request.getMemberHeight())
                 .memberWeight(request.getMemberWeight())
                 .personalInfoConsent(request.getPersonalInfoConsent())
-                .role(request.getRole())
+                .role(MemberRole.USER.toString())
                 .build();
 
         memberRepository.save(member);
-        return member;
+        return MemberRegisterResponse.builder().memberName(request.getMemberName()).build();
     }
 
 }
