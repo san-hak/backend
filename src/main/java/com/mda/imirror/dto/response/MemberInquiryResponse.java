aa3bf78a2af778fc7f10f390ee87885800fa6ad3
성의근 <103028914+HyunGyeol06@users.noreply.github.com>
2024-06-03 13:48:53 +0900

    member response changed.

--- src/main/java/com/mda/imirror/dto/response/MemberInquiryResponse.java
+++ src/main/java/com/mda/imirror/dto/response/MemberInquiryResponse.java
@@ -9,12 +9,19 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MemberInquiryResponse {
     @Schema(description = "이름", example = "홍길동")
     private String memberName;
+
     @Schema(description = "성별")
     private String memberGender;
+
     @Schema(description = "생년월일", example = "1997-05-04")
     private LocalDate memberBirthDate;
+
+    @Schema(description = "최근 검사일")
+    private LocalDate recentTestDate;
 
 }
