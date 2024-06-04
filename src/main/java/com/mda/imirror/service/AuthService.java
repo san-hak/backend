d3ef7bacf4bb405c28e32f6123d65d788675202d
성의근 <103028914+HyunGyeol06@users.noreply.github.com>
2024-06-04 12:37:18 +0900

    we don't use Polymorphism, so we don't need service interface and serviceImpl.

--- src/main/java/com/mda/imirror/service/AuthService.java
+++ src/main/java/com/mda/imirror/service/AuthService.java
@@ -1,15 +1,67 @@
 package com.mda.imirror.service;
 
+import com.mda.imirror.domain.entity.Member;
+import com.mda.imirror.domain.enums.MemberRole;
 import com.mda.imirror.dto.request.MemberLoginRequest;
 import com.mda.imirror.dto.request.MemberRegisterRequest;
 import com.mda.imirror.dto.response.MemberLoginResponse;
 import com.mda.imirror.dto.response.MemberRegisterResponse;
+import com.mda.imirror.exception.DuplicateMemberException;
+import com.mda.imirror.exception.MemberNotFoundException;
+import com.mda.imirror.exception.UnAuthorizedException;
+import com.mda.imirror.repository.MemberRepository;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public interface AuthService {
-    MemberRegisterResponse register(MemberRegisterRequest request);
+@Service
+@RequiredArgsConstructor
+public class AuthService {
 
-    MemberLoginResponse login(MemberLoginRequest request, HttpServletRequest httpServletRequest);
+    private final MemberRepository memberRepository;
+    public MemberRegisterResponse register(MemberRegisterRequest request) {
 
-    void logout(HttpServletRequest request);
+        Member member = Member.builder()
+                .memberName(request.getMemberName())
+                .memberBirthDate(request.getMemberBirthDate())
+                .isMale(request.getIsMale())
+                .personalInfoConsent(request.getPersonalInfoConsent())
+                .role(MemberRole.USER.toString())
+                .build();
+
+        if (memberRepository.existsByMemberNameAndMemberBirthDate(request.getMemberName(), request.getMemberBirthDate())) {
+            throw new DuplicateMemberException();
+        }
+        memberRepository.save(member);
+        return MemberRegisterResponse.builder()
+                .memberName(request.getMemberName())
+                .memberBirthDate(request.getMemberBirthDate())
+                .build();
+    }
+
+    public MemberLoginResponse login(MemberLoginRequest request, HttpServletRequest httpServletRequest) {
+        Member member = memberRepository.findByMemberNameAndMemberBirthDate(request.getMemberName(), request.getMemberBirthDate())
+                .orElseThrow(MemberNotFoundException::new);
+
+        HttpSession session = httpServletRequest.getSession(true);
+        session.setAttribute("member", member);
+
+
+        return MemberLoginResponse.builder()
+                .memberName(member.getMemberName())
+                .memberGender(member.getIsMale())
+                .memberBirthDate(member.getMemberBirthDate())
+                .role(member.getRole())
+                .build();
+    }
+
+    public void logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        } else {
+            throw new UnAuthorizedException();
+        }
+    }
 }
--- src/main/java/com/mda/imirror/service/CheckupService.java
+++ src/main/java/com/mda/imirror/service/CheckupService.java
@@ -1,14 +1,98 @@
 package com.mda.imirror.service;
 
+import com.mda.imirror.domain.entity.Balance;
 import com.mda.imirror.domain.entity.Member;
+import com.mda.imirror.domain.entity.Rom;
+import com.mda.imirror.dto.mapper.impl.BalanceMapper;
+import com.mda.imirror.dto.mapper.impl.RomMapper;
 import com.mda.imirror.dto.request.CheckupResultRequest;
 import com.mda.imirror.dto.request.MemberNameAndBirthRequest;
 import com.mda.imirror.dto.response.CheckupResultResponse;
+import com.mda.imirror.exception.MemberNotFoundException;
+import com.mda.imirror.repository.BalanceRepository;
+import com.mda.imirror.repository.MemberRepository;
+import com.mda.imirror.repository.RomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
-public interface CheckupService {
-    public void registerCheckupResult(CheckupResultRequest request);
-    List<CheckupResultResponse> getCheckupResult(MemberNameAndBirthRequest request);
-    public List<CheckupResultResponse> getCheckupResult(Member member);
+@Service
+@RequiredArgsConstructor
+public class CheckupService {
+
+    private final MemberRepository memberRepository;
+    private final RomRepository romRepository;
+    private final BalanceRepository balanceRepository;
+    private final AuthService authService;
+
+    public void registerCheckupResult(CheckupResultRequest request) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        LocalDate date = LocalDate.parse(request.getMemberBirthDate(), formatter);
+
+
+
+        Member member= memberRepository.findByMemberNameAndMemberBirthDate(request.getMemberName(),date)
+                .orElseGet(() -> Member.builder()
+                        .memberPk(UUID.randomUUID().toString())
+                        .memberName(request.getMemberName())
+                        .memberBirthDate( LocalDate.parse(request.getMemberBirthDate()))
+                        .role("ROLE_USER")
+                        .personalInfoConsent(true)  //임시
+                        .isMale(true)
+                        .build());
+
+
+//        Rom rom = RomMapper.MAPPER.toEntity(request.getRom());
+//        Balance balance = BalanceMapper.MAPPER.toEntity(request.getBalance());
+
+        Rom rom = Rom.builder()
+                .build();
+
+        Balance balance = Balance.builder()
+                .build();
+        member.changeMemberInfo(
+                null,
+                null,
+                null,
+                null,
+                LocalDate.now()
+
+        );
+
+        memberRepository.save(member);
+        romRepository.save(rom);
+        balanceRepository.save(balance);
+    }
+
+    public List<CheckupResultResponse> getCheckupResult(MemberNameAndBirthRequest request) {
+        Member member = memberRepository.findByMemberNameAndMemberBirthDate(request.getName(), request.birthToLocalDate(request.getBirth()))
+                .orElseThrow(MemberNotFoundException::new);
+
+        return getCheckupResultResponses(member);
+    }
+
+    public List<CheckupResultResponse> getCheckupResult(Member member) {
+        return getCheckupResultResponses(member);
+    }
+
+    private List<CheckupResultResponse> getCheckupResultResponses(Member member) {
+        List<Rom> roms = romRepository.findTop10ByMemberOrderByRomPkDesc(member);
+        List<Balance> balances = balanceRepository.findTop10ByMemberOrderByBalancePkDesc(member);
+
+        List<CheckupResultResponse> response = new ArrayList<>();
+        int responseSize = Math.min(roms.size(), Math.min(balances.size(), 10));
+
+        for (int i = 0; i < responseSize; i++) {
+            response.add(new CheckupResultResponse(
+                    RomMapper.MAPPER.toDto(roms.get(i)),
+                    BalanceMapper.MAPPER.toDto(balances.get(i))
+            ));
+        }
+        return response;
+    }
 }
